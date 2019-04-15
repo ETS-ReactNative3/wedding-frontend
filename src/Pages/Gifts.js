c0cc4feb63139b0e7d127138fca89bfd35c9a73e
Jeroen Cuvelier <jeroen.cuvelier@live.be>
2019-04-15 20:50:47 +0200

    more cleanup

--- src/Pages/Gifts.js
+++ src/Pages/Gifts.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import Header from '../Components/Header'
 
-import weddingDress from '../Assets/gifts/weddingDress.png'
-import food from '../Assets/gifts/food.png'
-import weddingParty from '../Assets/gifts/weddingParty.png'
-import airplanes from '../Assets/gifts/airplanes.png'
+import takeoff from '../Assets/gifts/takeoff.png'
+import landing from '../Assets/gifts/landing.png'
+import nz from '../Assets/gifts/nz.jpg'
+import vietnam from '../Assets/gifts/vietnam.jpg'
 
 class Gifts extends Component {
   render() {
@@ -12,16 +12,23 @@ class Gifts extends Component {
       <div className="App">
         <Header />
         <div className='text-div'>
-          Zoals jullie al weten, wonen wij ondertussen al meer dan 2 jaar samen. In ons huis hebben wij alles waar we gelukkig van worden. (Vooral Magnusje, natuurlijk.)
+          Zoals jullie al weten, wonen wij ondertussen meer dan 2 jaar samen in ons eigen huis.<br />
+          Ons huis staat al vol met potten & pannen, boeken, dozen vol Magic-kaarten & onze kat. Kortom, we hebben al alles om gelukkig te zijn.
         </div>
         <div className='text-div'>
-          Cadeau's geven is daarom ook totaal niet nodig. Wil je toch een cadeau geven, willen we vriendelijk vragen om ons dan gewoon de monetaire waarde van je cadeau te storten. Hieronder volgt een lijstje trouwgerelateerde uitgaven, waar je ons mee zou helpen.
+          Wel plannen we om in het najaar een mooie huwelijksreis te maken naar de andere kant van de wereld. <br />
+          Als je iets wil geven, kan je ons veel plezier doen met een centje voor onze grote reis.
         </div>
+        <h1>Waar kan je ons mee helpen:</h1>
         <div className='gifts-wrapper'>
-          <div className='gift-div'><img className='gift-img' src={weddingDress} /><span className='gift-description'>Een trouwjurk uit de sprookjes.</span></div>
-          <div className='gift-div'><img className='gift-img' src={food} /><span className='gift-description'>Een heerlijke maaltijd om met onze vrienden en familie te delen.</span></div>
-          <div className='gift-div'><img className='gift-img' src={weddingParty} /><span className='gift-description'>Het feest van ons leven om de dag van ons leven te vieren.</span></div>
-          <div className='gift-div'><img className='gift-img' src={airplanes} /><span className='gift-description'>Een huwelijksreis.</span></div>
+          <div className='gift-div'><img className='gift-img' src={takeoff} /><span className='gift-description'>Een huwelijksreis.</span></div>
+          <div className='gift-div'><img className='gift-img flag-img' src={nz} /><span className='gift-description'>Rondreizen in Nieuw Zeeland.</span></div>
+          <div className='gift-div'><img className='gift-img flag-img' src={vietnam} /><span className='gift-description'>Genieten in Vietnam.</span></div>
+          <div className='gift-div'><img className='gift-img' src={landing} /><span className='gift-description'>Een veilige vlucht terug naar huis.</span></div>
+          <div className='gift-div'></div>
+          <div className='gift-div'><img className='gift-img flag-img' src={nz} /><span className='gift-description'>10 dagen rondreizen in Nieuw Zeeland.</span></div>
+          <div className='gift-div'><img className='gift-img flag-img' src={vietnam} /><span className='gift-description'>Een weekje genieten in Vietnam.</span></div>
+          <div className='gift-div'></div>
         </div>
       </div>
     )
